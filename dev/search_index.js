5d6bf5b52a343885590367d03c97b7cfc32b7e43
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-08-28 17:04:44 +0000

    build based on 535fc27

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SeisNetwork.jl-1","page":"Home","title":"SeisNetwork.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [SeisNetwork]","category":"page"}]
+}
